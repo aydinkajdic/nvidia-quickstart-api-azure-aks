6faee332237597ce9bbd9e21f97116b47dca2cbe
Aydin Kajdic <aydinkajdic@microsoft.com>
2024-04-16 21:18:09 +0200

    Debugging workflow

--- backend.tf
+++ backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG-AK-AI-DEV-NE-001"
-    storage_account_name = "stgautomationfortaoapi"
-    container_name       = "automation-for-tao-api"
-    key                  = "terraform.tfstate"
   }
 }
