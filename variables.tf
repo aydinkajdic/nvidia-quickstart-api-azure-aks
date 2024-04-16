9d7141bc03fb0e5011f8639fefef37f956c935e3
Aydin Kajdic <aydinkajdic@microsoft.com>
2024-04-16 21:57:56 +0200

    Debugging workflow

--- variables.tf
+++ variables.tf
@@ -1,10 +1,24 @@
-variable "provider_config" {
-  type = object({
-    tenant_id       = string
-    subscription_id = string
-    client_id       = string
-    client_secret   = string
-  })
+# variable "provider_config" {
+#   type = object({
+#     tenant_id       = string
+#     subscription_id = string
+#     client_id       = string
+#     client_secret   = string
+#   })
+# }
+
+variable "tenant_id" {
+  type = string
+}
+variable "subscription_id" {
+  type = string
+}
+variable "client_id" {
+  type = string
+}
+variable "client_secret" {
+  type      = string
+  sensitive = true
 }
 variable "name" {
   type = string
