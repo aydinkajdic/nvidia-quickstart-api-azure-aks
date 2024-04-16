8572011b5ccc9c6c3cb7da00f646ebee92ba16e6
Aydin Kajdic <aydinkajdic@microsoft.com>
2024-04-16 20:52:51 +0200

    Debugging workflow

--- backend.tf
+++ backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = "terraform.tfstate"
   }
 }
--- variables.tf
+++ variables.tf
@@ -55,3 +55,15 @@ variable "chart_values" {
   type = map(string)
   description = "Values for the chart"
 }
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
